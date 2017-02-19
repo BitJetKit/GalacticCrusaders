0269502a87195d2b007fe569764d3a5e456c1cb0
Vish B-K <vishkarthikeyan@gmail.com>
2017-02-18 23:28:47 -0600

    More development on Spaceship
    More setup on Panel, and integrated 2D Graphics. KetEvents created.
    Spaceship cleaned up and added draw and moveShip methods.

--- src/Panel.java
+++ src/Panel.java
@@ -1,11 +1,13 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
@@ -14,23 +16,32 @@ import javax.swing.JPanel;
 public class Panel extends JPanel implements KeyListener {
 	static BufferedImage img;
 	static BufferedImage img2;
+	static BufferedImage img3;
 	static int imgY;
 	
 	static int screenWidth = 1280;
 	static int screenHeight = 720;
 	int frameRate = 200;
+	
+	int lvl = 0;
 
 	boolean isPause = false;
+	boolean isLeft, isRight;
+	
+	ArrayList<Alien> aliens;
+	Spaceship p1;
 
 	public Panel() {
 		imgY = 0;
 		try {
 			img = ImageIO.read(new File("Starfield.png"));
 			img2 = img;
+			img3 = img;
 		} catch (IOException e) {
 			System.out.println(e);
 		}
 		
+		p1 = new Spaceship(2, 2, 1);
 		setPreferredSize(new Dimension(screenWidth, screenHeight));
 		setBackground(Color.BLACK);
 		addKeyListener(this);
@@ -38,23 +49,32 @@ public class Panel extends JPanel implements KeyListener {
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D) g;
 		g.drawImage(img, 0, imgY, screenWidth, screenHeight, null);
 		g.drawImage(img2, 0, imgY - screenHeight, screenWidth, screenHeight, null);
+		g.drawImage(img2, 0, imgY - screenHeight * 2, screenWidth, screenHeight, null);
+		
+		p1.draw(g2);
 	}
 
 	public void run() {
 
 		while (!isPause) {
-			imgY++;
+			imgY += 4;
 			if (imgY >= screenHeight) {
+				System.out.println(imgY);
 				imgY = 0;
 			}
+			
+			p1.update();
+			if (isRight) p1.moveShip(1);
+			if (isLeft)  p1.moveShip(-1);
 			repaint();
 			delay(1000 / frameRate);
 		}
 	}
 
-	public void delay(int n) {
+	public void delay (int n) {
 		try {
 			Thread.sleep(n);
 		} catch (InterruptedException ex) {
@@ -63,21 +83,21 @@ public class Panel extends JPanel implements KeyListener {
 	}
 
 	@Override
-	public void keyPressed(KeyEvent e) {
-		if (e.getKeyChar() == 'w') {
-			
-		}
+	public void keyPressed (KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) isRight = true;
+		
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) isLeft = true;
 	}
 
 	@Override
-	public void keyReleased(KeyEvent e) {
+	public void keyReleased (KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) isRight = false;
 		
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) isLeft = false;
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		if (e.getKeyChar() == 'p') {
-
-		}
+		
 	}
 }
--- src/Spaceship.java
+++ src/Spaceship.java
@@ -1,36 +1,45 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
 import java.awt.Rectangle;
 
 public class Spaceship {
 	
-	int xPos;
-	int yPos;
-	int height;
-	int width;
-	int gunHeight;
-	int gunWidth;
 	Rectangle body;
-	Rectangle gBody;
+	Rectangle gunBody;
 	int bulletSpeed;
 	int speed;
 	int health;
 	
 	public Spaceship() {
-		xPos = Panel.screenWidth/2;
-		yPos = 40;
+		body    = new Rectangle (Panel.screenWidth/2, Panel.screenHeight - 20, 50, 15);
+		gunBody = new Rectangle ((body.x + body.width/2) - 12/2, body.y - (25-body.height), 12, 25);
 		bulletSpeed = 2;
-		speed = 2;
+		speed       = 2;
 		health = 1;
 	}
 	
-	public Spaceship (int x, int y, int bs, int s, int h) {
-		height = 5;
-		width = 10;
-		gunHeight = 8;
-		gunWidth = 6;
-		body  = new Rectangle (x, y, width, height);
-		gBody = new Rectangle (x/2, y - (gunHeight-height), gunWidth, gunHeight);
+	public Spaceship (int bs, int s, int h) {
+		body    = new Rectangle (Panel.screenWidth/2, Panel.screenHeight - 20, 50, 15);
+		gunBody = new Rectangle ((int)(body.getCenterX() - 6), body.y - (25 - body.height), 12, 25);
 		bulletSpeed = bs;
-		speed = s;
+		speed       = s;
 		health = h;
+	}
+	
+	public void draw (Graphics2D g) {
+		g.setColor(Color.RED);
+		g.fill(body);
+		g.fill(gunBody);
+		//g.drawRect(xPos, yPos, gunWidth, gunHeight);
+	}
+	
+	public void update () {
+		
+	}
+	
+	public void moveShip (int multiplier) {
+			body.x    += speed * multiplier;
+			gunBody.x += speed * multiplier;
 	}
 }
